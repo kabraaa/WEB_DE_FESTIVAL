cc8bbc261e0ed71b71e04c70d8d6ce958a52b14e
kabraaa <loredop05@gmail.com>
2025-11-05 03:33:57 +0100

    legends-grid js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,56 @@
-const legendsCards = document.querySelectorAll('.legend-card');
+// Selecciona todos los personajes
+const legendCards = document.querySelectorAll('.legend-card');
 const modal = document.createElement('div');
 
+// Crea estructura modal
+modal.id = 'legendModal';
+modal.innerHTML = `
+  <div class="modal-content">
+    <span class="close-modal">&times;</span>
+    <img src="" alt="">
+    <h3></h3>
+    <p></p>
+  </div>
+`;
+document.body.appendChild(modal);
+
+// Referencias internas
+const modalImg = modal.querySelector('img');
+const modalName = modal.querySelector('h3');
+const modalInfo = modal.querySelector('p');
+const closeModalBtn = modal.querySelector('.close-modal');
+
+// Abrir modal al hacer click
+legendCards.forEach(card => {
+  card.addEventListener('click', () => {
+    const name = card.getAttribute('data-name');
+    const info = card.getAttribute('data-info');
+    const image = card.getAttribute('data-image');
+
+    modalImg.src = image;
+    modalImg.alt = name;
+    modalName.textContent = name;
+    modalInfo.textContent = info;
+
+    modal.classList.add('show');
+  });
+});
+
+// Cerrar modal aspa
+closeModalBtn.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
+
+// Cerrar modal pulsar fuera
+window.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.classList.remove('show');
+  }
+});
+
+// Cerrar con esc
+window.addEventListener('keydown', e => {
+  if (e.key === 'Escape') {
+    modal.classList.remove('show');
+  }
+});
